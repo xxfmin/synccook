2d6a67aafdd88b3a31dbf79624fd11de76b85c89
Felipe Min <felipemin03@gmail.com>
2025-06-10 15:34:14 -0400

    added names to users

--- models/User.ts
+++ models/User.ts
@@ -2,6 +2,8 @@ import mongoose, { Schema, InferSchemaType, Model } from "mongoose";
 
 const UserSchema = new Schema(
   {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
