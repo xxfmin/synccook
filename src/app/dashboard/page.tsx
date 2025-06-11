befc81a7f4d370c96161ca2a778e6c036b3a12d1
Felipe Min <felipemin03@gmail.com>
2025-06-11 16:22:09 -0400

    incorporated navbar + sidebar block

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import headerSvg from "@/../public/img/dashboard/svg-bg.png";
+import DashNavbar from "../../../components/DashNavbar";
+import { SidebarDemo } from "../../../components/DashSidebar";
+
+export default function Dashboard() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") return <p>Loading…</p>;
+  if (!session) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="min-h-screen bg-emerald-100">
+      <div className="relative h-80 w-full ">
+        <Image
+          src={headerSvg}
+          alt="Dashboard header"
+          fill
+          quality={100}
+          className="object-cover"
+        />
+
+        <div className="absolute top-0 w-full z-10 px-4 ">
+          <div className="max-w-7xl mx-auto w-full pb-20 ">
+            <DashNavbar />
+            <h1 className="text-emerald-200 text-3xl font-semibold py-10 ">
+              Welcome back, {session.user.firstName}!
+            </h1>
+            <SidebarDemo />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
