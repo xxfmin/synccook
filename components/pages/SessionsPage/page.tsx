74aeb47ec46b80f48c46a40ecd00af9e9a630286
Felipe Min <felipemin03@gmail.com>
2025-06-11 22:59:17 -0400

    added base layout

--- components/pages/SessionsPage/page.tsx
+++ components/pages/SessionsPage/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const SessionsPage = () => {
   return (
-    <div>SessionsPage</div>
-  )
-}
+    <div className="flex flex-col w-full h-full">
+      <div className="flex flex-row justify-between">
+        <h1 className="text-gray-700 font-bold text-2xl">Cooking Sessions</h1>
+        <button className="bg-teal-600 hover:bg-teal-700 text-white px-3 py-2 rounded cursor-pointer">
+          New Session
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default SessionsPage
+export default SessionsPage;
